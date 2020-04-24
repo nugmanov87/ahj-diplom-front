e4ce4532b3148bea140247234a1fd774b95a427c
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2020-04-24 20:50:44 +0500

    пробный вебпак

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+
+const merge = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const common = require("./webpack.config.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["file-loader?name=[name].[ext]", "extract-loader", "html-loader"],
+      },
+    ],
+  },
+});
