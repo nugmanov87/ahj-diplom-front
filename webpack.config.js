e7c0066792e73414c1b343b1a27a1417a3d20aae
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2020-04-24 22:22:07 +0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     publicPath: "/dist",
   },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
   module: {
     rules: [
       {
